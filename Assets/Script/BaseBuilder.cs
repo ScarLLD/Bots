b01298db1012472c510a0e4a0ff52f7a331bc822
ScarLLD <www.olegborisov1@gmail.com>
2024-04-01 16:53:09 +0300

    2.0

--- Assets/Script/BaseBuilder.cs
+++ Assets/Script/BaseBuilder.cs
@@ -1,16 +1,21 @@
 using System.Collections;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(BaseCollector))]
 public class BaseBuilder : MonoBehaviour
 {
     [SerializeField] private int _basePrice;
-    [SerializeField] private GameObject _flagPrefab;
+    [SerializeField] private Flag _flagPrefab;
     [SerializeField] private Base _basePrefab;
-    [SerializeField] private Wallet _wallet;
     [SerializeField] private Camera _camera;
+    [SerializeField] private Wallet _wallet;
+    [SerializeField] private LayerMask _hitMask;
 
-    private bool isBuilding = false;
+    private Coroutine _templateCoroutine;
+    private Collider[] _colliders;
+    private bool _isBuildingTemplate = false;
+
 
     public void Interect()
     {
@@ -19,7 +24,8 @@ public class BaseBuilder : MonoBehaviour
 
     private void ShowTemplate()
     {
-        StartCoroutine(RenderTemplate());
+        if (_templateCoroutine == null)
+            _templateCoroutine = StartCoroutine(RenderTemplate());
     }
 
     private void BuildBase(Vector3 position)
@@ -33,27 +39,34 @@ public class BaseBuilder : MonoBehaviour
 
     private IEnumerator RenderTemplate()
     {
-        isBuilding = true;
+        _isBuildingTemplate = true;
 
-        GameObject flag = Instantiate(_flagPrefab);
+        Flag flag = Instantiate(_flagPrefab);
 
-        while (isBuilding)
+        RaycastHit hit;
+        Ray ray;
+
+        while (_isBuildingTemplate)
         {
-            RaycastHit hit;
-            Ray ray = (_camera.ScreenPointToRay(Input.mousePosition));
+            ray = (_camera.ScreenPointToRay(Input.mousePosition));
 
-            Debug.DrawRay(ray.origin, ray.direction);
+            Debug.DrawRay(ray.origin, ray.direction * 100);
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 100f, _hitMask))
             {
-                Debug.Log("collider ray");
+                flag.transform.position = hit.point + new Vector3(0, 0.1f, 0);
 
-                flag.transform.position = hit.point + new Vector3(0, 0, 1);
+                _colliders = Physics.OverlapBox(flag.transform.position, flag.transform.localScale * 6, Quaternion.identity);
 
-                if (Input.GetMouseButtonDown(0) == true)
+                if (_colliders.All(collider => collider.GetComponent<LevelPlane>()))
+                    flag. .SetActive(true);
+                else
+                    flag.gameObject.SetActive(false);
+
+                if (Input.GetMouseButtonDown(0) == true && flag.gameObject.activeInHierarchy == true)
                 {
                     BuildBase(hit.transform.position);
-                    isBuilding = false;
+                    _isBuildingTemplate = false;
                 }
             }
 
--- Assets/Script/BaseCollector.cs
+++ Assets/Script/BaseCollector.cs
@@ -5,12 +5,14 @@ public class BaseCollector : MonoBehaviour
     [SerializeField] private ResourcePool _resourcePool;
     [SerializeField] private Wallet _wallet;
     [SerializeField] private Base _basePrefab;
+    [SerializeField] private Vector3 _startPosition;
 
+    public Vector3 GetStartPosition => _startPosition;
     public Wallet GetWallet => _wallet;
     public ResourcePool GetResourcePool => _resourcePool;
 
     private void Awake()
     {
-        Instantiate(_basePrefab, transform);
+        Instantiate(_basePrefab, _startPosition, Quaternion.identity, transform);
     }
 }
